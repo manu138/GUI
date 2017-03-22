aa8cca8d1bbb6af327b115c219a8679299ebcca0
Manuela Recio <manu138@hotmail.es>
2017-03-22 13:18:24 -0500

    primer commit

---
+++ Assets/Scripts/Mostrar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mostrar : MonoBehaviour {
+    public List<InventoryItemList> itemList;
+    InventoryItem item;
+
+    // Use this for initialization
+    void Start () {
+        itemList.Add( Resources.Load("InventoryItemList") as InventoryItemList);
+        //  itemList.Add(Resources.Load("data") as InventoryItemList);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(itemList);
+    }
+}
